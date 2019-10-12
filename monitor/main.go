420e8600fcb5a5e88a1f97f1d84e832d5b1e2cbf
Syamala Umamaheswaran <shykart2203@gmail.com>
2019-10-13 00:51:56 +0200

    reorder imports

--- monitor/main.go
+++ monitor/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"github.com/shyamz-22/monitor/internal/exceptions"
 	"log"
 	"net/http"
 	"os"
@@ -14,6 +13,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/shyamz-22/monitor/internal/exceptions"
 )
 
 const separator = ","
